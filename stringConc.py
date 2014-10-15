44c88e29e7badadd70ff4fd49c44da459b216809
Sokratis Tsakiltsidis <sokratis1308@gmail.com>
2014-10-15 14:22:36 -0400

    Adding stringConc.py in master

---
+++ stringConc.py
@@ -0,0 +1,23 @@
+import yappi
+
+a = "Sok"
+b = "Tsak"
+c = "Lok"
+d = "Fok"
+
+yappi.set_clock_type('cpu')
+
+def slow(a, b, c, d):
+	print a + b + c + d
+
+def fast(a, b, c, d):
+	print "%s%s%s%s" % (a, b, c, d)
+
+yappi.start(builtins=True, profile_threads=True)
+
+slow(a, b, c, d)
+
+fast(a, b, c, d)
+
+yappi.stop()   
+yappi.get_func_stats().print_all()
