807d1d92464e1890b1a70b2b9004fe3685db3644
Sokratis Tsakiltsidis <sokratis1308@gmail.com>
2014-10-16 11:29:07 -0400

    comments

--- stringConc.py
+++ stringConc.py
@@ -8,7 +8,7 @@ c = "Lok"
 d = "Fok"
 e = "Yikes"
 
-yappi.set_clock_type('cpu')
+#yappi.set_clock_type('cpu')
 
 def slow(a, b, c, d):
 	print a + b + c + d
@@ -16,7 +16,7 @@ def slow(a, b, c, d):
 def fast(a, b, c, d):
 	print "%s%s%s%s" % (a, b, c, d)
 
-yappi.start(builtins=False, profile_threads=True)
+#yappi.start(builtins=False, profile_threads=True)
 
 print "The slow is: "
 slow(a, b, c, d)
@@ -24,5 +24,5 @@ slow(a, b, c, d)
 print "The fast is: "
 fast(a, b, c, d)
 
-yappi.stop()   
-yappi.get_func_stats().print_all()
+#yappi.stop()   
+#yappi.get_func_stats().print_all()
